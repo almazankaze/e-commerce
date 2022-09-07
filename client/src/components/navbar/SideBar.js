935fbdb99a624d5bb7f1525586ac181d1d32f061
John Almazan <almazankaze@live.com>
2022-09-06 22:21:51 -0500

    styled sidebar

--- client/src/components/navbar/SideBar.js
+++ client/src/components/navbar/SideBar.js
@@ -1,5 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PermIdentityOutlinedIcon from "@mui/icons-material/PermIdentityOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import HardwareOutlinedIcon from "@mui/icons-material/HardwareOutlined";
+import MouseOutlinedIcon from "@mui/icons-material/MouseOutlined";
+import MonitorOutlinedIcon from "@mui/icons-material/MonitorOutlined";
+import PrintOutlinedIcon from "@mui/icons-material/PrintOutlined";
+import SmartphoneOutlinedIcon from "@mui/icons-material/SmartphoneOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import PhoneOutlinedIcon from "@mui/icons-material/PhoneOutlined";
+import ComputerOutlinedIcon from "@mui/icons-material/ComputerOutlined";
+import ArrowDropDownOutlinedIcon from "@mui/icons-material/ArrowDropDownOutlined";
 
 import { useGlobalContext } from "../../context";
 
@@ -10,66 +23,74 @@ const SideBar = () => {
 
   return (
     <div className={showMenu ? "sidebar show" : "sidebar"}>
+      <div className="sidebar-logo-container">
+        <h2>MeTech</h2>
+      </div>
       <ul className="sidebar-menu">
         <li>
-          <Link className="link" to="/home">
-            Home
+          <Link className="link sidebar-item" to="/home">
+            <HomeOutlinedIcon className="sidebar-icon" /> Home
           </Link>
         </li>
         <li>
-          <Link className="link" to="/home">
-            My Account
+          <Link className="link sidebar-item" to="/home">
+            <PermIdentityOutlinedIcon className="sidebar-icon" /> My Account
           </Link>
         </li>
         <li>
-          <Link className="link" to="/home">
-            Wishlist
+          <Link className="link sidebar-item" to="/home">
+            <FavoriteBorderOutlinedIcon className="sidebar-icon" /> Wishlist
           </Link>
+          <div className="side-badge">9+</div>
         </li>
         <li>
-          <Link className="link" to="/home">
-            Shopping Cart
+          <Link className="link sidebar-item" to="/home">
+            <ShoppingCartOutlinedIcon className="sidebar-icon" /> Shopping Cart
           </Link>
+          <div className="side-badge">9+</div>
         </li>
-        <li>
-          <Link className="link" to="/home">
-            Hardware
-          </Link>
+        <li className="side-dropdown">
+          <div className="link sidebar-item">
+            <HardwareOutlinedIcon className="sidebar-icon" /> Hardware
+          </div>
+          <ArrowDropDownOutlinedIcon className="fill-main-color" />
         </li>
-        <li>
-          <Link className="link" to="/home">
-            Peripherals
-          </Link>
+        <li className="side-dropdown">
+          <div className="link sidebar-item" to="/home">
+            <MouseOutlinedIcon className="sidebar-icon" /> Peripherals
+          </div>
+          <ArrowDropDownOutlinedIcon className="fill-main-color" />
         </li>
-        <li>
-          <Link className="link" to="/home">
-            Computers
-          </Link>
+        <li className="side-dropdown">
+          <div className="link sidebar-item" to="/home">
+            <ComputerOutlinedIcon className="sidebar-icon" /> Computers
+          </div>
+          <ArrowDropDownOutlinedIcon className="fill-main-color" />
         </li>
 
         <li>
-          <Link className="link" to="/home">
-            Monitors
+          <Link className="link sidebar-item" to="/home">
+            <MonitorOutlinedIcon className="sidebar-icon" /> Monitors
           </Link>
         </li>
         <li>
-          <Link className="link" to="/home">
-            Printers
+          <Link className="link sidebar-item" to="/home">
+            <PrintOutlinedIcon className="sidebar-icon" /> Printers
           </Link>
         </li>
         <li>
-          <Link className="link" to="/home">
-            Smartphones
+          <Link className="link sidebar-item" to="/home">
+            <SmartphoneOutlinedIcon className="sidebar-icon" /> Smartphones
           </Link>
         </li>
         <li>
-          <Link className="link" to="/home">
-            FAQ's
+          <Link className="link sidebar-item" to="/home">
+            <HelpOutlineOutlinedIcon className="sidebar-icon" /> FAQ's
           </Link>
         </li>
         <li>
-          <Link className="link" to="/home">
-            Contact Us
+          <Link className="link sidebar-item" to="/home">
+            <PhoneOutlinedIcon className="sidebar-icon" /> Contact Us
           </Link>
         </li>
       </ul>
