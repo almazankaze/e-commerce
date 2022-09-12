793b78603d01cabd33c312b55044297ba0640c54
John Almazan <almazankaze@live.com>
2022-09-11 20:49:48 -0500

    added basic image card

---
+++ client/src/components/imageCard/ImageCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./imageCard.css";
+
+function ImageCard() {
+  return (
+    <div className="imageCard-container">
+      <div className="imageCard-background">
+        <div className="imageCard-content">
+          <h3>Armed Pc Gamer</h3>
+          <p>Choose your pc and play!</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ImageCard;
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
 import PicCarousel from "../../components/PicCarousel/PicCarousel";
+import ImageCard from "../../components/imageCard/ImageCard";
+
+import "./home.css";
 
 const Home = () => {
   return (
     <div className="container">
-      <PicCarousel />
+      <div className="home-banner-container">
+        <PicCarousel />
+        <ImageCard />
+      </div>
     </div>
   );
 };
