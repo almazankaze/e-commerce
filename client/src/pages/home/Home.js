6b801463202cdc3b657cb96ded79f5366668ee81
John Almazan <almazankaze@live.com>
2022-09-16 00:04:44 -0500

    started finished perks section

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,6 +1,10 @@
 import React from "react";
 import PicCarousel from "../../components/PicCarousel/PicCarousel";
 import ImageCard from "../../components/imageCard/ImageCard";
+import LocalShippingOutlinedIcon from "@mui/icons-material/LocalShippingOutlined";
+import GppGoodOutlinedIcon from "@mui/icons-material/GppGoodOutlined";
+import HeadsetMicOutlinedIcon from "@mui/icons-material/HeadsetMicOutlined";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
 
 import "./home.css";
 
@@ -13,6 +17,36 @@ const Home = () => {
       </div>
       <div className="home-perks-container">
         <div className="perk-card">
+          <div className="perk-icon">
+            <LocalShippingOutlinedIcon />
+          </div>
+          <div className="perk-info">
+            <h4>Free Shipping</h4>
+            <p className="desc-text">Orders over $100</p>
+          </div>
+        </div>
+        <div className="perk-card">
+          <div className="perk-icon">
+            <AccountBalanceWalletOutlinedIcon />
+          </div>
+          <div className="perk-info">
+            <h4>Free Shipping</h4>
+            <p className="desc-text">Orders over $100</p>
+          </div>
+        </div>
+        <div className="perk-card">
+          <div className="perk-icon">
+            <GppGoodOutlinedIcon />
+          </div>
+          <div className="perk-info">
+            <h4>Free Shipping</h4>
+            <p className="desc-text">Orders over $100</p>
+          </div>
+        </div>
+        <div className="perk-card">
+          <div className="perk-icon">
+            <HeadsetMicOutlinedIcon />
+          </div>
           <div className="perk-info">
             <h4>Free Shipping</h4>
             <p className="desc-text">Orders over $100</p>
