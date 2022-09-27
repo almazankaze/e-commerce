ced567569ea2acbfdc12e95df3fc878cd1991dfb
John Almazan <almazankaze@live.com>
2022-09-26 21:59:24 -0500

    added categories section

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import PicCarousel from "../../components/PicCarousel/PicCarousel";
 import ImageCard from "../../components/imageCard/ImageCard";
 import LocalShippingOutlinedIcon from "@mui/icons-material/LocalShippingOutlined";
@@ -13,11 +14,15 @@ import note from "../../img/notebooks/notebooks.jpg";
 import phone from "../../img/phones/smartphones.jpg";
 
 import "./home.css";
+import "swiper/css";
+import "swiper/css/pagination";
 import Slider from "../../components/slider/Slider";
 
 import { data } from "../../data/offers.js";
 import { notebooks } from "../../data/notebooks.js";
 import { phones } from "../../data/phones.js";
+
+import { Pagination } from "swiper";
 
 const Home = () => {
   return (
@@ -120,6 +125,77 @@ const Home = () => {
       <section className="home-section">
         <h1>Categories</h1>
         <hr></hr>
+
+        <div className="my-custom-pagination-div"></div>
+
+        <Swiper
+          pagination={{
+            el: ".my-custom-pagination-div",
+            clickable: true,
+            renderBullet: (index, className) => {
+              return "<span class=" + className + "></span>";
+            },
+          }}
+          modules={[Pagination]}
+          className="categoriesSwiper"
+        >
+          <SwiperSlide>
+            <div className="category-slide">
+              <div className="category-cell">
+                <p>Video Cards</p>
+              </div>
+              <div className="category-cell">
+                <p>Power Supplies</p>
+              </div>
+              <div className="category-cell">
+                <p>Motherboard</p>
+              </div>
+              <div className="category-cell">
+                <p>Processors</p>
+              </div>
+              <div className="category-cell">
+                <p>Memory Ram</p>
+              </div>
+              <div className="category-cell">
+                <p>Hard Drives</p>
+              </div>
+              <div className="category-cell">
+                <p>SSD</p>
+              </div>
+              <div className="category-cell">
+                <p>Cases</p>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="category-slide">
+              <div className="category-cell">
+                <p>Mice</p>
+              </div>
+              <div className="category-cell">
+                <p>Keyboards</p>
+              </div>
+              <div className="category-cell">
+                <p>Printers</p>
+              </div>
+              <div className="category-cell">
+                <p>Notebooks</p>
+              </div>
+              <div className="category-cell">
+                <p>Phones</p>
+              </div>
+              <div className="category-cell">
+                <p>Tablets</p>
+              </div>
+              <div className="category-cell">
+                <p>Monitors</p>
+              </div>
+              <div className="category-cell">
+                <p>Pendrives</p>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </section>
     </div>
   );
